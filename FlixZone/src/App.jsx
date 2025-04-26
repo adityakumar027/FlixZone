0f1d0f9ccf8860c9af9c638eadc8d635520cf261
Aditya <adi.workspace76865@gmail.com>
2025-04-26 23:58:37 +0530

    increased data

--- FlixZone/src/App.jsx
+++ FlixZone/src/App.jsx
@@ -13,9 +13,10 @@ const App = () =>{
             <h2 className='bg-red'> Functional Arrow Component </h2>
             <Card title = "The Star Wars"/>
             <Card title = "Avatars"/>
-            <Card title = "The Lion King" > < /Card>
+            <Card title = "The Lion King" > </Card>
             <Card  title = "The Lion kKing" />
             <Card title  = "ADITYA THE GREAT"> </Card>
+            <Card title  = "LABBBHAD THE GREAT"> </Card>
         </div>
         
     )
